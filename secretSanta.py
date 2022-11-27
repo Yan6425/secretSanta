68908005834b576e05c60639a824c2a43bc0a3cd
Yan <yan.h-h@orange.fr>
2022-11-27 23:05:20 +0100

    Plus que le CSS.

--- secretSanta.py
+++ secretSanta.py
@@ -1,34 +1,70 @@
 from json import load, dump
 from random import randint
+from flask import render_template
+import smtplib, ssl
+from email.message import EmailMessage
 
 
 def envoyerMail(secretSanta, participant):
-    print(f"Bravo {secretSanta}, tu es le secret Santa de {participant}.")
+    msg = EmailMessage()
+    msg.set_content(render_template('mail.html', utilisateur = secretSanta, participant = participant))
+    msg["Subject"] = "Ta participation au secret Santa !"
+    msg["From"] = "secretsanta@orange.fr"
+    msg["To"] = "deuxiemecomptedeyan@gmail.com"
+    context=ssl.create_default_context()
+
+    with smtplib.SMTP("smtp.orange.fr", port=587) as smtp:
+        smtp.starttls(context=context)
+        smtp.login("yan.youtube@orange.fr", "Bearnais64!")
+        smtp.send_message(msg)
+
+
+def obtenirEmail(participant):
+    if participant == 'inconnu':
+        return
+    with open('emails.json') as fichierJson:
+        dicoEmails = load(fichierJson)
+    return dicoEmails[participant]
+
+
+def tirageFini():
+    return peutCommencer()
+
+
+def peutCommencer():
+    with open('participants.json') as fichierJson:
+        dicoParticipants = load(fichierJson)
+
+    res = True
+    for confirmation in dicoParticipants.values():
+        if confirmation is None:
+            res = False
+    return res
 
 
 def tirageAuSort():
     with open('participants.json') as fichierJson:
         dicoParticipants = load(fichierJson)
 
-    peutCommencer = True
-    for confirmation in dicoParticipants.values():
-        if confirmation is None:
-            peutCommencer = False
-
-    if peutCommencer:
+    if peutCommencer():
         listeParticipants = list(dicoParticipants)
         for participant in dicoParticipants:
-            envoyerMail(participant, listeParticipants.pop(randint(0, len(listeParticipants) - 1)))
+            participant2 = listeParticipants.pop(randint(0, len(listeParticipants) - 1))
+            envoyerMail(participant, participant2)
+
+
+def déjàValidé(participant):
+    with open('participants.json') as fichierJson:
+        dicoParticipants = load(fichierJson)
+
+    return dicoParticipants[participant] is not None
 
 
 def validation(participant, réponse):
     with open('participants.json') as fichierJson:
         dicoParticipants = load(fichierJson)
     
-    if dicoParticipants[participant] is not None:
-        return "déjàValidé"
-    else:
-        dicoParticipants[participant] = réponse
+    dicoParticipants[participant] = réponse
 
     with open('participants.json', 'w') as fichierJson:
         dump(dicoParticipants, fichierJson)
@@ -63,3 +99,4 @@ def enregistrerEmail(utilisateur, email):
             dicoEmails[utilisateur] = email
         with open('emails.json', 'w') as fichierJson:
             dump(dicoEmails, fichierJson)
+        return True
