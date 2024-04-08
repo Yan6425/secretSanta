14e54a1018bf81a058c8eeb4fa3c12f2781cdf95
Yan <yan.hourdebaigt.hiroux@gmail.com>
2024-04-08 09:05:26 +0200

    bug résolu lancerTirage

--- scripts/lancerTirage.php
+++ scripts/lancerTirage.php
@@ -1,15 +1,23 @@
 <?php
 $participants = json_decode(file_get_contents("../participants.json"), true);
-$tblNoms = array_keys($participants);
-foreach ($participants as $nom => $infos) {
-    if ($nom != "Admin") {
-        do {
-            $i = random_int(1, count($tblNoms)-1);
-        } while (in_array($tblNoms[$i], $infos["blackList"]));
-        $participants[$nom]["secretEnfant"] = $tblNoms[$i];
-        array_splice($tblNoms, $i, 1);
+do {
+    $tblNoms = array_keys($participants);
+    array_splice($tblNoms, 0, 1);
+    $boucleInfinie = false;
+    foreach ($participants as $nom => $infos) {
+        if ($nom != "Admin") {
+            if (empty(array_diff($tblNoms, $infos["blackList"]))){
+                $boucleInfinie = true;
+                break;
+            }
+            do {
+                $i = random_int(0, count($tblNoms)-1);
+            } while (in_array($tblNoms[$i], $infos["blackList"]));
+            $participants[$nom]["secretEnfant"] = $tblNoms[$i];
+            array_splice($tblNoms, $i, 1);
+        }
     }
-}
+} while ($boucleInfinie);
 file_put_contents("../participants.json", json_encode($participants, JSON_PRETTY_PRINT));
 
 $parametres = json_decode(file_get_contents("../parametres.json"), true);
