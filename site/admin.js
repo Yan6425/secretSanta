d5b47d28648751fa5d1e08fdddc91d39fa6d755e
Yan6425 <yan.h-h@orange.fr>
2025-03-25 15:53:36 +0100

    page admin liste participants et tirage[B

--- site/admin.js
+++ site/admin.js
@@ -1,26 +1,26 @@
 function lancerTirage(){
     fetch("../fonctions/lancerTirage.php")
     .then(response => {
-    if (!response.ok) {
-        throw new Error(`Erreur HTTP : ${response.status}`);
-    }
-    return response.text();
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP : ${response.status}`);
+        }
+        window.location.reload();
     })
     .catch(error => {
-    console.error('Erreur lors du tirage :', error);
+        console.error('Erreur lors du tirage :', error);
     });
 }
 
 function annulerTirage(){
     fetch("../fonctions/annulerTirage.php")
     .then(response => {
-    if (!response.ok) {
-        throw new Error(`Erreur HTTP : ${response.status}`);
-    }
-    return response.text();
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP : ${response.status}`);
+        }
+        window.location.reload();
     })
     .catch(error => {
-    console.error('Erreur lors du tirage :', error);
+        console.error('Erreur lors du tirage :', error);
     });
 }
 
--- site/pages/admin.php
+++ site/pages/admin.php
@@ -3,9 +3,26 @@ session_start();
 if (!isset($_SESSION["pseudo"]) || $_SESSION["pseudo"] != "Admin"){
     header("Location: ../index.php");
 }
+$participants = json_decode(file_get_contents('../../participants.json'), true);
+
 include "../header.html"
 ?>
-azerty
+<button onclick="lancerTirage()">Lancer tirage</button>
+<button onclick="annulerTirage()">Annuler tirage</button>
+<table>
+    <tr>
+        <th>Pseudo</th>
+        <th>Email</th>
+        <th>Secret enfant</th>
+    </tr>
+    <?php foreach ($participants as $nom => $infos): ?>
+        <tr>
+            <td><?= htmlspecialchars($nom) ?></td>
+            <td><?= htmlspecialchars($infos['mail']) ?></td>
+            <td><?= htmlspecialchars($infos['secretEnfant']) ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
 <script type="text/javascript" src="../admin.js"></script>
 <?php
 include "../footer.html"
