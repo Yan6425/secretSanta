8213947f623be7fba5b48d052ff649b19761143a
Yan <yan.h-h@orange.fr>
2025-03-27 11:43:53 +0100

    correction dernier commit

--- site/fonctions/lancerTirage.php
+++ site/fonctions/lancerTirage.php
@@ -1,23 +1,27 @@
 <?php
 $participants = json_decode(file_get_contents("../../participants.json"), true);
 do {
-    $tblNoms = array_keys($participants);
-    array_splice($tblNoms, 0, 1);
-    $boucleInfinie = false;
-    foreach ($participants as $pseudo => $infos) {
-        if ($pseudo != "Admin") {
-            if (empty(array_diff($tblNoms, $infos["blackList"]))){
-                $boucleInfinie = true;
-                break;
-            }
-            do {
-                $i = random_int(0, count($tblNoms)-1);
-            } while (in_array($tblNoms[$i], $infos["blackList"]));
-            $participants[$pseudo]["secretEnfant"] = $tblNoms[$i];
-            array_splice($tblNoms, $i, 1);
+    $tblPseudos = array_keys($participants);
+    $tblPseudos = array_values(array_diff($tblPseudos, ["Admin"]));
+    foreach($participants as $pseudo => $infos) {
+        if ($infos["banni"]) {
+            $tblPseudos = array_values(array_diff($tblPseudos, [$peudo]));
         }
     }
-} while ($boucleInfinie);
+    $reessayer = false;
+    foreach ($tblPseudos as $pseudo) {
+        if (empty(array_diff($tblPseudos, $participants[$pseudo]["blackList"]))){
+            // il ne reste plus que des personnes blacklist pour cet utilisateur, il faut reessayer
+            $reessayer = true;
+            break;
+        }
+        do {
+            $i = random_int(0, count($tblPseudos)-1);
+        } while (in_array($tblPseudos[$i], $participants[$pseudo]["blackList"]));
+        $participants[$pseudo]["secretEnfant"] = $tblPseudos[$i];
+        array_splice($tblPseudos, $i, 1);
+    }
+} while ($reessayer);
 file_put_contents("../../participants.json", json_encode($participants, JSON_PRETTY_PRINT));
 
 $parametres = json_decode(file_get_contents("../../parametres.json"), true);
