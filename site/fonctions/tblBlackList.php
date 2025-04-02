ff8bcebfa58ffd3fa65cdef636799894286f3810
Yan <yan.h-h@orange.fr>
2025-04-02 15:32:25 +0200

    bannis non affichés dans BL

--- site/fonctions/tblBlackList.php
+++ site/fonctions/tblBlackList.php
@@ -3,7 +3,7 @@ $participants = json_decode(file_get_contents("../../participants.json"), true);
 if (isset($_POST["pseudo"])                                         //vérifie que les identifiants sont définis
     && array_key_exists($_POST["pseudo"], $participants)){          //vérifie que l'utilisateur existe
         foreach ($participants as $pseudo => $infos) {
-            if ($pseudo != "Admin" && $pseudo != $_POST["pseudo"]) : ?>
+            if ($pseudo != "Admin" && $pseudo != $_POST["pseudo"] && !$infos["banni"]) : ?>
                 <tr>
                     <td><?= htmlspecialchars($pseudo) ?></td>
                     <td><?= htmlspecialchars($infos['mail']) ?></td>
